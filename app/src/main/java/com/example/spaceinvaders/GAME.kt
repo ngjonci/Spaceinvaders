a81e5c940bdf8780d4c852c9ff5cc98ac9de1ae4
NexieSlowo <62484829+NexieSlowo@users.noreply.github.com>
2022-04-07 16:50:12 +0200

    Space Invaders version1: → opening game menu (MainActivity.kt -- activity_main.xml) → clicking on start button in game menu sends to the game screen (SecondActvity.kt -- activity_second.xml) → note: GAME.kt and activity_game.xml isn't implemented because it doesn't work for unknown reason. Thus SecondActivity.kt is a copy of GMAE.kt deprived of the part of the code causing crash.

---
+++ app/src/main/java/com/example/spaceinvaders/GAME.kt
@@ -0,0 +1,55 @@
+package com.example.spaceinvaders
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.Toast
+
+class GAME : AppCompatActivity() {
+
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_botom_anim) }
+    private var clicked = false
+    private val floatingActionButton1 = findViewById<Button>(R.id.floatingActionButton1)
+    private val floatingActionButton2 = findViewById<Button>(R.id.floatingActionButton2)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+
+        floatingActionButton1.setOnClickListener {
+            onfloatingActionButton()
+        }
+        floatingActionButton2.setOnClickListener {
+            Toast.makeText(this, "RESTART", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun onfloatingActionButton() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        clicked = !clicked
+    }
+    private fun setVisibility(clicked: Boolean) {
+        if (!clicked){
+            floatingActionButton2.visibility = View.VISIBLE
+        }else{
+            floatingActionButton2.visibility = View.INVISIBLE
+        }
+    }
+    private fun setAnimation(clicked: Boolean) {
+        if (!clicked){
+            floatingActionButton2.startAnimation(fromBottom)
+            floatingActionButton1.startAnimation(rotateOpen)
+        }else {
+            floatingActionButton2.startAnimation(toBottom)
+            floatingActionButton1.startAnimation(rotateClose)
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/spaceinvaders/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.spaceinvaders
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val changeActivityBTN = findViewById<Button>(R.id.button1)
+        changeActivityBTN.setOnClickListener{
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
