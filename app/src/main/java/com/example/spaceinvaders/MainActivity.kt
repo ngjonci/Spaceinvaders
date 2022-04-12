d555332c54a715921c22797bacbc178586b6eb2c
NexieSlowo <62484829+NexieSlowo@users.noreply.github.com>
2022-04-12 19:54:31 +0200

    TEST CHANGE

--- app/src/main/java/com/example/spaceinvaders/MainActivity.kt
+++ app/src/main/java/com/example/spaceinvaders/MainActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-/*
-TEST
- */
+
 class MainActivity : AppCompatActivity() {
 
 
