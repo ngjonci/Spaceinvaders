038f18c0a2eb1f1f0b3738cda790af8685b86e71
ngjonci <ngjonci12@gmail.com>
2022-04-18 20:02:30 +0200

    J'ai écrit un commentaireee :)

--- app/src/main/java/com/example/spaceinvaders/GAME.kt
+++ app/src/main/java/com/example/spaceinvaders/GAME.kt
@@ -7,6 +7,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.Toast
+//Testttttttttttttt
 
 class GAME : AppCompatActivity() {
 
